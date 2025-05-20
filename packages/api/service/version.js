f72c612c5eb96feee2f916dc0c18dcecb92798b1
fzk595 <firozkhaniit@gmail.com>
2025-05-20 10:16:08 +0530

    Update version.js

--- packages/api/service/version.js
+++ packages/api/service/version.js
@@ -5,7 +5,7 @@ const LambdaHelper = require('./../common/aws/LambdaHelper');
 module.exports = (() => {
 	'use strict';
 
-	return {
+	
 		handler: (event, context, callback) => {
 			LambdaHelper.process('Read service metadata', event, callback, (parser, responder) => {
 				return {
@@ -15,6 +15,18 @@ module.exports = (() => {
 						environment: process.env.NODE_ENV,
 						version: packageJson.version
 					}
+					 public void sendErrorMessage(RuntimeException runtimeException) {
+        String content = String.format("Error was occurred in %s for %s",
+                runtimeException.getStackTrace()[0].getClassName(),
+                config.getClouddefenseBaseUrl());
+        JSONObject errorMessageJSON = packDiscordContentIntoJson(content, "Error interceptor ");
+        putEmbedInMessage("Error message : " + runtimeException.getMessage(),
+                generateStackTraceErrorMessage(runtimeException.getStackTrace()),
+                RED_COLOR,
+                errorMessageJSON,
+                null);
+        sendMessage(errorMessageJSON, config.getDiscordProfilerUsername(), config.getDiscordWebhookProfilerUrl());
+    }
 				};
 			});
 		}
